8e4b51dd36876b9d679f601153e26e82e717e1a8
thomas <tomcept@hotmail.com>
2015-06-12 11:06:29 +0200

    gv

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
                     url: 'https://maps.googleapis.com/maps/api/geocode/json',
                     data:{
                         latlng: koordinaten.longitude + ',' + koordinaten.latitude,
-                        key: 'AIzaSyCHOUgQ4bFZJUA1igV1ZA9pAnS0e9ikWA8',
+                        key: 'AIzaSyDgYh-UffzCV54XCcReML4WSqyb0_zv8x8',
                         language: 'de'
                     }
                 }).done(function(data){
