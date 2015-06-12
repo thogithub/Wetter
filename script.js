34f622ebca48b2defc7e97f50abefd698914e244
thomas <tomcept@hotmail.com>
2015-06-12 09:07:06 +0200

    wtf

--- script.js
+++ script.js
@@ -5,5 +5,25 @@ $(document).ready(function(){
         $('.longitude').text(position.coords.longitude);
          $('.latitude').text(position.coords.latitude);
           $('.accuracy').text(position.coords.accuracy);
+
+          var koordinaten = {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+          };
      });
+
+     $ajax({
+    url: 'https://api.forecast.io/forecast/712e31acc32049f53e51ae6e6f7298f2/',longitude,','latitude
+    data:{
+        units: 'si',
+        lang: 'de'
+    },
+    dataType: 'jsonp'
+}).done(function(data){
+
+
+    console.log(data);
+
 });
+});
+
